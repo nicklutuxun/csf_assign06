17a25e3c743c36615bbde973869deb8393cb452c
Nick Lu <nicklu@nicklu.acasmart.jh.edu>
2021-12-08 23:19:00 -0500

    Add comments

--- calcServer.c
+++ calcServer.c
@@ -6,16 +6,30 @@
 
 int chat_with_client(struct Calc *calc, int client_fd);
 
+/**
+ * This is the connection information for one thread.
+ * Every thread will have their own ConnInfo
+ * 
+ * @param clientfd The file descriptor of client 
+ * @param calc The pointer to the shared Calc struct
+ */
 struct ConnInfo {
 	int clientfd;
 	struct Calc *calc;
 };
 
+
+/**
+ * The function executed when pthread_create is called
+ * 
+ * @param arg ConnInfo passed in
+ * @return void* 
+ */
 void *worker(void *arg) {
 	struct ConnInfo *info = arg;
-	pthread_detach(pthread_self());
-	chat_with_client(info->calc, info->clientfd);
-	close(info->clientfd);
+	pthread_detach(pthread_self());		// Let the client threads be detached so that the server does not wait for it to complete
+	chat_with_client(info->calc, info->clientfd);		// interact with the server
+	close(info->clientfd);		// close the client thread
 	free(info);
 
 	return NULL;
@@ -26,48 +40,54 @@ int main(int argc, char **argv) {
 		exit(0); 	// incorrect number of command line argument
 	}
 
-	struct Calc *calc = calc_create();
+	struct Calc *calc = calc_create();		// create calc and initialize pthread mutex
 	const char *port = argv[1];
 
-	int server_fd = open_listenfd((char*) port);
+	int server_fd = open_listenfd((char*) port);		// Open and return a listening socket on port for the server
 	if (server_fd < 0) { return 0; } // fatal error
 
 	int keep_going = 1;
 	while (keep_going) {
-		int client_fd = Accept(server_fd, NULL, NULL);
+		int client_fd = Accept(server_fd, NULL, NULL);		// establish client connection with the server
 		if (client_fd < 0) { 
 			printf("Fatal: Error accepting client connection");
 			return 0;
 		}		// fatal: Error accepting client connection
 		
-		struct ConnInfo *info = malloc(sizeof(struct ConnInfo));
+		struct ConnInfo *info = malloc(sizeof(struct ConnInfo));		// reserve memory for ConnInfo
 		info->clientfd = client_fd;
 		info->calc = calc;
 
 		pthread_t thr_id;
+
+		// create thread and execute worker function
 		if (pthread_create(&thr_id, NULL, worker, info) != 0)
 		{
 			printf("Fatal: pthread_create failed");
 			return 0;		 // fatal: pthread_create failed
 		}
 	}
-	close(server_fd);
+	close(server_fd);		// close server file descriptor
 
-	calc_destroy(calc);
+	calc_destroy(calc);		// delete calc and destory pthread mutex
 	return 0;
 }
 
+/**
+ * Read lines of input, evaluate them as calculator expressions,
+ * and (if evaluation was successful) print the result of each
+ * expression.  Quit when "quit" command is received.
+ * 
+ * @param calc The shared Calc struct
+ * @param client_fd client file descriptor
+ * @return int 
+ */
 int chat_with_client(struct Calc *calc, int client_fd) {
 	rio_t in;
 	char linebuf[LINEBUF_SIZE];
 
 	rio_readinitb(&in, client_fd);
 
-	/*
-	 * Read lines of input, evaluate them as calculator expressions,
-	 * and (if evaluation was successful) print the result of each
-	 * expression.  Quit when "quit" command is received.
-	 */
 	int done = 0;
 	while (!done) {
 		ssize_t n = rio_readlineb(&in, linebuf, LINEBUF_SIZE);
